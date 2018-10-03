f513261c030db41f79505891bd87ab01bfd8fb6b
Cheunkie <w.f.lam@live.nl>
2018-10-04 00:35:28 +0200

    merging steps

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,101 @@
-console.log("Script Loaded!");
+////////console.log("Script Loaded!");
+////////
+////////console.log("Hello World!"); 
+////////var firstName = 'John';
+////////console.log(firstName);
+////////var lastName = 'Smith';
+////////var age = 38;
+////////
+////////var fullAge = true;
+////////console.log(fullAge)
+//////var x, y;
+//////x = y = (3+5)*4 -6;
+//////x = x * 2;
+////////x *= 2;
+//////Precendence, Associativity
+//////console.log(x);
+////
+////
+//////var markHeight = 1.80;
+//////var markMass = 70;
+//////var johnHeight = 1.90;
+//////var johnMass = 60;
+//////
+//////var markBMI = markMass / (markHeight * markHeight);
+//////var johnBMI = johnMass / (johnHeight * johnHeight);
+//////
+//////if (markBMI > johnBMI) {
+//////    console.log('mark\'s BMI is higher then John ' + markBMI);
+//////}else{
+//////    console.log('john\'s BMI is higher then mark ' + johnBMI);
+//////                }
+////
+////var firstName = 'john';
+//var age = 12;
+////if (age < 16){
+////    console.log(firstName + ' is a boy.');
+////}else if (age >= 13 && age <= 20){
+////    console.log(firstName + ' is a teenager.');
+////}else if(age >= 20 && age < 30){
+////    console.log(firstName + ' is a young man.')
+////}else{
+////    console.log(firstName + ' is a man.');
+////}
+//
+//var firstName = 'john';
+////var age = 26;
+////
+////age >= 16 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+////
+////var drink = age >= 18 ? 'beer' : 'juice';
+////console.log(drink);
+////
+////if (age >= 18){
+////    var 
+////    
+////}
+//
+//
+//var job = 'teacher';
+//switch (job) {
+//    case 'teacher':
+//        console.log(firstName + ' teaches kids how to code.');
+//        break;
+//    case 'driver':
+//        console.log(firstName + ' drives an uber in Lisabon.');
+//        break;
+//    case 'designer':
+//        console.log(firstName + ' designs beautifulwebsites');
+//        break;
+//        
+//    default:
+//        console.log(firstName + ' does something else.');
+//}
+//
+//
+//switch(true){
+//    case age < 13:
+//        console.log(firstName + ' is a boy.');
+//        break;
+//    case age >=13 && age <20:
+//        console.log(firstName + ' is a teenager.');
+//        break;
+//    case age >= 20 && age < 30:
+//        console.log(firstName + ' is a young man.');
+//        break;
+//    default:
+//        console.log(firstName + ' is a man.');
+//}
 
-console.log("Hello World!"); 
+
+
+//falsy values: undefined, null, 0, '' ,NaN
+//truthy values: not falsy values
+
+//var height;
+//
+//if(height){
+//    console.log('Variable is defined.')
+//}else{
+//    console.log('Variable is not defined.');
+//}
